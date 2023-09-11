c9f432721efb41919a00b0817a5060493b924a13
NM2509 <73057662+NM2509@users.noreply.github.com>
2023-09-11 08:31:16 +0100

    Update 1. Simulations main function.py

--- 1. Simulations main function.py
+++ 1. Simulations main function.py
@@ -19,18 +19,6 @@ from sympy import Symbol
 from scipy.stats import kstest
 from scipy.optimize import minimize
 warnings.filterwarnings("ignore")
-    
-
-# Re-writing cumulative function using numba for time efficiency
-
-@jit(nopython=True, fastmath=True)
-def cumsum_numba(a):
-    out = np.empty_like(a)
-    cumsum = 0
-    for i in range(a.shape[0]):
-        cumsum += a[i]
-        out[i] = cumsum
-    return out
 
 # Function for choosing a node (through index in a list of nodes)
 
