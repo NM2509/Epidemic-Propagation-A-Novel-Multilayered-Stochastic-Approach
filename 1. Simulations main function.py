f4cd73f12e419ccf6844469753e1a5d238195841
NM2509 <73057662+NM2509@users.noreply.github.com>
2023-09-11 08:28:09 +0100

    Update 1. Simulations main function.py

--- 1. Simulations main function.py
+++ 1. Simulations main function.py
@@ -24,13 +24,6 @@ warnings.filterwarnings("ignore")
 # Other functions used in simulations #
 #######################################
 
-# Function: getting cluster by id
-
-def get_cluster_by_id(clusters, parent_id):
-    matching_clusters = [cluster for cluster in clusters 
-                         if cluster.cluster_id == parent_id]
-    return matching_clusters[0] if matching_clusters else 'Error'
-
 # Function for choosing an event (birth, infection, recovery, confirmation)
 
 events = ['birth','infection','recovery', 'confirmation']
