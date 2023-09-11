51041ff973e263f07e71647ce570c09f7db8c7cd
NM2509 <73057662+NM2509@users.noreply.github.com>
2023-09-11 08:33:34 +0100

    Update 1. Simulations main function.py

--- 1. Simulations main function.py
+++ 1. Simulations main function.py
@@ -20,23 +20,6 @@ from scipy.stats import kstest
 from scipy.optimize import minimize
 warnings.filterwarnings("ignore")
 
-# Function for choosing a node (through index in a list of nodes)
-
-def choose_index_of_node(probabilities, 
-                         random_numbers, 
-                         iteration_index):
-    
-    # cumulative sum of the probabilities
-    cumulative_probabilities = cumsum_numba(probabilities)
-    
-    # random number between 0 and 1
-    random_number = random_numbers[iteration_index]
-    
-    # where this number is inserted in the cumulative sum array
-    index = np.searchsorted(cumulative_probabilities, random_number)
-    
-    return index
-
 #####################################
 # FIRST MAIN FUNCTION - simulations #
 #####################################
